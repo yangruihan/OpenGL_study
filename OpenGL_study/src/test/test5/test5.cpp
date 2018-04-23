09597261a433eb75aec6f0452d2293676c0def68
C.y <yangruihan@vip.qq.com>
2018-04-23 13:46:23 +0800

    camera

--- OpenGL_study/src/test/test5/test5.cpp
+++ OpenGL_study/src/test/test5/test5.cpp
@@ -26,9 +26,72 @@ void process_input(GLFWwindow *window)
         camera_pos += glm::normalize(glm::cross(camera_front, camera_up)) * camera_speed;
 }
 
+float yaw;
+float pitch;
+float mouse_last_x;
+float mouse_last_y;
+bool first;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (first)
+    {
+        mouse_last_x = xpos;
+        mouse_last_y = ypos;
+        first = false;
+    }
+
+    float xoffset = xpos - mouse_last_x;
+    float yoffset = mouse_last_y - ypos; // 注意这里是相反的，因为y坐标是从底部往顶部依次增大的
+    mouse_last_x = xpos;
+    mouse_last_y = ypos;
+
+    const auto sensitivity = 0.15f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw   += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 60.0f)
+        pitch = 60.0f;
+    if (pitch < -60.0f)
+        pitch = -60.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
+    front.y = sin(glm::radians(pitch));
+    front.z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
+    camera_front = glm::normalize(front);
+}
+
+float fov;
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    const auto sensitivity = 5.0f;
+    if (fov >= 1.0f && fov <= 120.0f)
+        fov -= yoffset * sensitivity;
+    if (fov <= 1.0f)
+        fov = 1.0f;
+    if (fov >= 120.0f)
+        fov = 120.0f;
+}
+
 int main()
 {
     Window window(480, 480, "test4");
+
+    // set mouse mode
+    glfwSetInputMode(window.get_window(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    // add mouse callback
+    glfwSetCursorPosCallback(window.get_window(), mouse_callback);
+    first = true;
+
+    glfwSetScrollCallback(window.get_window(), scroll_callback);
+
+    mouse_last_x = 240.0f;
+    mouse_last_y = 240.0f;
 
     float pos[] = {
         -100.0f, -100.0f,  100.0f,  1.0f,  1.0f,  1.0f,
@@ -51,7 +114,7 @@ int main()
         7, 4, 6,
         6, 5, 4,
         4, 7, 0,
-        0, 2, 4,
+        0, 1, 4,
         4, 5, 1,
         1, 2, 5,
     };
@@ -64,7 +127,8 @@ int main()
     IndexBuffer index_buffer(index, 3 * 12);
     vertex_array.add_buffer(vertex_buffer, vertex_buffer_layout, index_buffer);
 
-    const auto proj = glm::perspective(glm::radians(90.0f), 1.0f, 0.1f, 480.0f);
+    fov = 90;
+    auto proj = glm::perspective(glm::radians(fov), 1.0f, 0.1f, 480.0f);
     auto view = glm::lookAt(camera_pos, camera_pos + camera_front, camera_up);
     auto model = glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 1.0f));
 
@@ -86,6 +150,7 @@ int main()
 
         renderer.clear();
 
+        proj = glm::perspective(glm::radians(fov), 1.0f, 0.1f, 480.0f);
         view = glm::lookAt(camera_pos, camera_pos + camera_front, camera_up);
         shader.set_uniform_mat4f("u_MVP", proj * view * model);
         renderer.draw(vertex_array, shader);
